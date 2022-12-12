62bb248870286a44dde69c7d0878831d91ee05ed
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-13 00:26:07 +0100

    sixth task

---
+++ src/main/java/models/BillCalculator.java
@@ -0,0 +1,14 @@
+package models;
+
+public class BillCalculator {
+
+    public double calculateBill(double billValue,float paymentForService ){
+        return billValue+paymentForService;
+    };
+    public double calculateBill(double billValue,float paymentForService,double discountInPercent ){
+        return billValue*(1-discountInPercent/100)+paymentForService;
+    };
+    public double calculateBill(double billValue,int paymentForService,short takeawayPackage ) {
+        return billValue + paymentForService + takeawayPackage;
+    }
+}
