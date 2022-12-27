b19b6841fbead3c810fe39a4dfe634267ac58ce8
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-27 22:44:38 +0100

    sixth task first correction

--- src/main/java/models/BillCalculator.java
+++ src/main/java/models/BillCalculator.java
@@ -2,13 +2,17 @@ package models;
 
 public class BillCalculator {
 
-    public double calculateBill(double billValue,float paymentForService ){
-        return billValue+paymentForService;
-    };
-    public double calculateBill(double billValue,float paymentForService,double discountInPercent ){
-        return billValue*(1-discountInPercent/100)+paymentForService;
-    };
-    public double calculateBill(double billValue,int paymentForService,short takeawayPackage ) {
-        return billValue + paymentForService + takeawayPackage;
+    public double calculateBill(double billValue, float paymentForService) {
+        return billValue + paymentForService;
+    }
+
+
+    public double calculateBill(double billValue, float paymentForService, double discountInPercent) {
+        return billValue * (1 - discountInPercent / 100) + paymentForService;
+    }
+
+
+    public double calculateBill(double billValue, int paymentForService, short paymentForTakeawayPackage) {
+        return billValue + paymentForService + paymentForTakeawayPackage;
     }
 }
