02dda92464111e1b9810fdde4880d5e70bc0513a
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-28 21:40:46 +0100

    ninth task

---
+++ src/main/java/apps/Main.java
@@ -0,0 +1,10 @@
+package apps;
+
+import models.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle1 = new Triangle(10, 7);
+        System.out.println(triangle1.toString());
+    }
+}
---
+++ src/main/java/models/Triangle.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Triangle {
+    private final double width;
+    private final double height;
+
+    public Triangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" + "width=" + width + ", height=" + height + '}';
+    }
+}
